5013ba4e96957ca57418e2792cd0e6148e29eec6
Anton Logunov <antoshkaplus@gmail.com>
2016-11-15 08:03:17 -0500

    graph templating

--- include/ant/graph/graph_2.hpp
+++ include/ant/graph/graph_2.hpp
@@ -33,7 +33,7 @@ class BellmanFord {
 
 
 // we have to know what dist returns
-template<class Graph, class Value>
+template<GraphDirection direction, class Value>
 class FloydWarshall {
     
     Grid<Value> dist_;
@@ -50,6 +50,7 @@ public:
     
     // should use both directions or only one...
     // use type_traits to resolve
+    template<EnableIf<std::is_same<direction, Iterator, ConstIterator>> = enabler>>
     void AddDist(int i_1, int i_2, Value val) {
         dist_(i_1, i_2) = dist_(i_2, i_1) = val;
     }
@@ -85,11 +86,118 @@ public:
 
 
 
-enum class GraphType {
+// when adding edges ... this one is important
+enum class GraphDirection {
     Directed,
     Undirected
+};
+
+enum class GraphConnectivity {
+    Sparse,
+    Dense
+};
+
+// but this thing only gives you if the edge there or not
+
+// short vs int
+// Node type
+
+// same stuff
+// Edge type
+
+// should edges be included or not?
+
+// 
+class GraphSparse {
+    vector<vector<T>> adj_list;
+
+    int node_count() {}
+
+    add_directed_edge()
+    
+    
+};
+
+// so those are like interfaces to everything
+// those interfaces can differ between each other
+
+class GraphDense {
+    Grid<int> mat;
+    
+    int node_count()
+
+    add_directed_edge()
+};
+
+
+// how to put inside edges vs no edges
+// the idea is that each edge or node can have information
+// for dense thing
+class GraphDenseDirected {
+    Grid<EdgeType> edges;
+};
+// but what we really want is probably keep them together as one item
+// this should create a lot of code duplication and we lose some functionality
+
+
+// we can always use functions instead of methods !!!!
+// why functions are so good?
+// you don't have subclass to add new functionality. 
+// it's easy to customize. 
+// but still basic interface is needed
+
+// maybe have object builder that would be friend to our usual classes but would call right
+// things for each one of them
+
+template<GraphConnectivity conn, class NodeType>
+void Add(Graph<GraphDirection::Directed, conn, NodeType>& gr, int from, int to) {
+    gr.add_directed_edge(from, to);
 }
 
+template<GraphConnectivity conn, class NodeType>
+void Add(Graph<GraphDirection::Undirected, conn, NodeType>& gr, int from, int to) {
+    gr.add_directed_edge(from, to);
+    gr.add_directed_edge(to, from);
+}
+
+// so we add two more methods those would take care adding stuff to EdgedGraphs
+// Add method should not be accessible. 
+
+// it may be useful to close implementation in that case
+
+// you go and specify protected inheritance or ....
+
+// yes we should do it another way
+
+
+template<GraphDirection direction, GraphConnectivity conn, class NodeType=int>
+class Graph {
+    
+};
+
+template<GraphDirection dir, GraphConnectivity conn, class NodeType=int, class EdgeType=int>
+// with same parameters
+class EdgedGraph : Graph<dir, conn, NodeType> {
+    
+    // you would have to overwrite some methods
+    
+    
+};
+
+
+template<GraphDirection dir, class NodeType=int, class EdgeType=int>
+class EdgedGraph : Graph<dir, GraphConnectivity::Dense, NodeType> {
+    // we consider from one to another
+    Grid<EdgeType> mat;
+    
+};
+
+template<GraphDirection dir, class NodeType=int, class EdgeType=int>
+class EdgedGraph : Graph<dir, GraphConnectivity::Sparse, NodeType> {
+    // we consider from one to another
+    vector<vector<Edges>> ;
+    // add is here
+};
 
 
 
@@ -97,9 +205,17 @@ enum class GraphType {
 // what if we want to store edges
 template<class NodeType, GraphType graph_type>
 class Graph {
-
-
 };
+
+
+
+template<bool Directed,  class NodeType>
+class Graph {};
+
+template<class NodeType>
+class Graph<true, NodeType> {
+
+}; 
 
 
 
