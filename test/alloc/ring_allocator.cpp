b70d6b68d6de3e4a5dcacc19478249ec4ac5b623
Anton Logunov <antoshkaplus@Antons-MacBook-Air.local>
2019-08-22 08:12:49 -0400

    gogo

---
+++ test/alloc/ring_allocator.cpp
@@ -0,0 +1,22 @@
+
+#include <fstream>
+#include <array>
+#include <functional>
+#include <set>
+
+#include "gtest/gtest.h"
+
+#include "ant/alloc/ring_allocator.hpp"
+
+namespace {
+
+    using namespace ant;
+    using namespace alloc;
+    using namespace std;
+
+
+    TEST(RingAllocator, allin) {
+        RingAllocator<int, 90, 90> hi;
+        std::vector<int, decltype(hi)> v(hi);
+    }
+}
