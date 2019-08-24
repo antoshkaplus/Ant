e36e3967d4e452f7e3f19e679b0196c652c7fe97
Anton Logunov <alogunov@bluefintrading.com>
2019-08-24 18:34:53 -0400

    gogo

--- include/ant/alloc/ring_resource.hpp
+++ include/ant/alloc/ring_resource.hpp
@@ -5,32 +5,37 @@
 namespace ant::alloc {
 
 // if out of alloc slots or buffer, allocate with standard alloc
-template <typename slot_size_type>
+
 class RingResource : public std::experimental::pmr::memory_resource {
 
     std::vector<char> buffer;
-    std::vector<slot_size_type> slots;
+    std::vector<uint32_t> slots;
 
-    slot_size_type slots_start = 0;
-    slot_size_type slots_taken = 0;
+    uint32_t slots_start = 0;
+    uint32_t slots_taken = 0;
 
     char* buffer_next_free;
 
     constexpr static char TAKEN = 1;
     constexpr static char EMPTY = 0;
 
-    slot_size_type max_dealloc_count;
+    uint32_t max_dealloc_count;
 
     size_t local_allocations_ = 0;
     size_t local_deallocations_ = 0;
+    size_t local_memory_releases_ = 0;
 
     size_t standard_allocations_ = 0;
     size_t standard_deallocations_ = 0;
 
 public:
-    RingResource(size_t buffer_size, slot_size_type slot_count, slot_size_type max_dealloc_count = 10)
+    RingResource(uint32_t buffer_size, uint32_t slot_count, uint32_t max_dealloc_count = 10)
         : buffer(buffer_size, EMPTY), slots(slot_count), buffer_next_free(buffer.data()),
-          max_dealloc_count(max_dealloc_count) {}
+          max_dealloc_count(max_dealloc_count) {
+
+        if (buffer_size <= 1) throw std::runtime_error("buffer smaller 2");
+        if (slots.size() == 0) throw std::runtime_error("slots count is 0");
+    }
 
     size_t standard_allocations() const {
         return standard_allocations_;
@@ -48,6 +53,9 @@ public:
         return local_deallocations_;
     }
 
+    size_t local_memory_releases() const {
+        return local_memory_releases_;
+    }
 
 protected:
 
@@ -63,6 +71,8 @@ protected:
 
         auto slots_start_ptr = buffer.data() + slots[slots_start];
 
+        assert(slots_start_ptr != buffer_next_free);
+
         if (buffer_next_free > slots_start_ptr)
         {
             return allocate_start_behind(sz, alignment);
@@ -74,22 +84,23 @@ protected:
     }
 
     void do_deallocate(void* ptr, std::size_t sz, std::size_t alignment) final {
-        if (ptr < buffer.data() && ptr >= buffer.data() + sz) {
+        if (ptr < buffer.data() || ptr >= buffer.data() + buffer.size()) {
             ++standard_deallocations_;
             std::experimental::pmr::new_delete_resource()->deallocate(ptr, sz, alignment);
             return;
         }
 
         reinterpret_cast<char*>(ptr)[-1] = EMPTY;
+        ++local_deallocations_;
 
         auto dealloc_count = 0;
         while ( slots_taken > 0 &&
-                slots[slots_start] == EMPTY &&
+                buffer[slots[slots_start]] == EMPTY &&
                 ++dealloc_count <= max_dealloc_count) {
 
-            ++local_deallocations_;
+            ++local_memory_releases_;
 
-            slots_start += (slots_start + 1) % slots.size();
+            slots_start = (slots_start + 1) % slots.size();
             --slots_taken;
         }
     }
@@ -102,16 +113,12 @@ private:
 
     void* allocate_no_slots_taken(std::size_t sz, std::size_t alignment) {
 
-        size_t remaining = 0;
+        // mark + buffer_next_free
+        size_t remaining = buffer.size() - 2;
         auto mark = buffer.data();
         auto alloc_start = mark + 1;
 
         if (!std::align(alignment, sz, (void*&)alloc_start, remaining)) {
-            return allocate_standard(sz, alignment);
-        }
-
-        if (alloc_start + sz >= buffer.data() + buffer.size())
-        {
             return allocate_standard(sz, alignment);
         }
 
@@ -122,21 +129,17 @@ private:
 
     void* allocate_start_behind(std::size_t sz, std::size_t alignment) {
 
-        size_t remaining = 0;
+        // size - taken items - 1 (mark) -1 (buffer_next_free)
+        size_t remaining = buffer.size() - (buffer_next_free - buffer.data()) - 2;
         auto mark = buffer_next_free;
         auto alloc_start = mark + 1;
 
         if (!std::align(alignment, sz, (void*&)alloc_start, remaining)) {
-            return allocate_standard(sz, alignment);
-        }
-
-        if (alloc_start + sz >= buffer.data() + buffer.size())
-        {
             if (slots[slots_start] == 0)
             {
                 return allocate_standard(sz, alignment);
             }
-            buffer_next_free = 0;
+            buffer_next_free = nullptr;
             return allocate_start_ahead(sz, alignment);
         }
 
@@ -148,16 +151,11 @@ private:
 
     void* allocate_start_ahead(std::size_t sz, std::size_t alignment) {
 
-        size_t remaining = 0;
+        size_t remaining = (buffer.data() + slots[slots_start]) - buffer_next_free - 2;
         auto mark = buffer_next_free;
         auto alloc_start = mark + 1;
 
         if (!std::align(alignment, sz, (void*&)alloc_start, remaining)) {
-            return allocate_standard(sz, alignment);
-        }
-
-        if (alloc_start + sz >= buffer.data() + slots[slots_start])
-        {
             return allocate_standard(sz, alignment);
         }
 
--- test/alloc/ring_resource.cpp
+++ test/alloc/ring_resource.cpp
@@ -3,9 +3,12 @@
 #include <array>
 #include <functional>
 #include <set>
+#include <random>
+#include <vector>
 
 #include "gtest/gtest.h"
 
+#include "ant/core/core.hpp"
 #include "ant/alloc/alloc.hpp"
 #include "ant/alloc/ring_resource.hpp"
 
@@ -16,22 +19,143 @@ using namespace alloc;
 using namespace std;
 
 
-TEST(RingResource, allin) {
-    RingResource<uint16_t> resource(2000 * 90, 1000);
+struct TestParams {
+    uint32_t slots;
+    uint32_t alloc_items;
+    uint32_t items;
+    uint32_t alloc_item_min;
+    uint32_t alloc_item_max;
+    uint32_t alloc_items_min_count;
+    uint32_t alloc_items_max_count;
+};
 
-    std::vector<alloc::vector<int>> vs(10, alloc::vector<int>(&resource));
-    for (auto i = 0; i < vs.size(); ++i) {
-        vs[i].resize(10, i);
+
+template <typename T>
+class RingResourceTest : public testing::TestWithParam<TestParams> {
+
+    std::vector<alloc::vector<T>> test;
+    std::vector<std::vector<T>> correct;
+
+    std::default_random_engine rng;
+    std::uniform_int_distribution<uint32_t> item_index_distr;
+    std::uniform_int_distribution<uint32_t> alloc_items_count_distr;
+    std::uniform_int_distribution<uint32_t> alloc_item_distr;
+
+    virtual void SetUp() {
+        auto param = GetParam();
+        test.assign(param.items, {});
+        correct.assign(param.items, {});
+
+        item_index_distr = std::uniform_int_distribution<uint32_t>{0, param.items - 1};
+        alloc_items_count_distr = std::uniform_int_distribution{
+            param.alloc_items_min_count, param.alloc_items_max_count};
+        alloc_item_distr = std::uniform_int_distribution{
+            param.alloc_item_min, param.alloc_item_max};
     }
 
-    for (auto i = 0; i < vs.size(); ++i) {
-        ASSERT_EQ(vs[i].size(), 10);
-        for (auto k : vs[i]) ASSERT_EQ(k, i);
+
+};
+
+
+
+using RingResourceTest_Types = ::testing::Types<int8_t, int32_t, int64_t, std::array<int64_t, 4>>;
+TYPED_TEST_SUITE(RingResourceTest, RingResourceTest_Types);
+
+
+void PrintAllocations(const RingResource& resource) {
+    auto str = ant::Format("local alloc: %d, dealloc: %d; standard alloc: %d, dealloc: %d",
+                           resource.local_allocations(), resource.local_deallocations(),
+                           resource.standard_allocations(), resource.standard_deallocations());
+    Println(std::cout, str);
+}
+
+struct RingResourceSize {
+    uint32_t buffer;
+    uint32_t slots;
+};
+
+
+TYPED_TEST(RingResourceTest, ZeroBufferSlots) {
+    std::vector<RingResourceSize> params = {{0, 0}, {0, 10}, {10, 0}, {1, 10}};
+    for (auto& p : params) {
+        try {
+            RingResource resource(p.buffer, p.slots);
+            DefaultResourceGuard guard(resource);
+            {
+                alloc::vector<TypeParam> arr;
+                arr.resize(10);
+            }
+        } catch (...) {}
     }
 }
 
-TEST(RingResource, _1) {
-    RingResource<uint16_t> resource(2000 * 90, 1000);
+TYPED_TEST(RingResourceTest, ContiniousAllocation) {
+    RingResource resource(2000 * 90, 1000);
+    DefaultResourceGuard guard(resource);
+    {
+        const auto kItemCount = 1000;
+    }
+}
+
+
+//
+//TEST(RingResourceTest, RandomVectorAssignment_Small) {
+//    RingResource resource(2000 * 90, 10);
+//    DefaultResourceGuard guard(resource);
+//    {
+//        const auto kItemCount = 1000;
+//        std::vector<alloc::vector<int>> test(kItemCount, alloc::vector<int>(&resource));
+//        std::vector<std::vector<int>> correct(kItemCount);
+//
+//        const auto kItem_2_CountMin = 0;
+//        const auto kItem_2_CountMax = 10;
+//
+//        const auto kItem_2_Min = 0;
+//        const auto kItem_2_Max = (int) 10e+6;
+//
+//        const auto iterations = (int) 10e+3;
+//    }
+//}
+
+
+
+TEST(RingResourceTest, RandomVectorAssignment_Big) {
+    RingResource resource(2000 * 90, 1000);
+    DefaultResourceGuard guard(resource);
+    {
+        const auto kItemCount = 1000;
+        std::vector<alloc::vector<int>> test(kItemCount, alloc::vector<int>(&resource));
+        std::vector<std::vector<int>> correct(kItemCount);
+
+        const auto kItem_2_CountMin = 0;
+        const auto kItem_2_CountMax = 10;
+
+        const auto kItem_2_Min = 0;
+        const auto kItem_2_Max = (int) 10e+6;
+
+        const auto iterations = (int) 10e+3;
+
+        for (auto i = 0; i < iterations; ++i) {
+            std::vector<int> new_item(count_distr(rng));
+            for (auto& elem : new_item) elem = item_distr(rng);
+
+            auto index = item_index_distr(rng);
+            test[index] = alloc::vector<int>();
+            test[index].assign(new_item.begin(), new_item.end());
+            correct[index] = new_item;
+
+            if (!std::equal(correct.begin(), correct.end(), test.begin())) {
+                PrintAllocations(resource);
+
+                ASSERT_TRUE(false);
+            }
+        }
+    }
+    PrintAllocations(resource);
+}
+
+TEST(RingResourceTest, _1) {
+    RingResource resource(2000 * 90, 1000);
 
     std::vector<alloc::vector<int>> vs(10, alloc::vector<int>(&resource));
     for (auto i = 0; i < vs.size(); ++i) {
