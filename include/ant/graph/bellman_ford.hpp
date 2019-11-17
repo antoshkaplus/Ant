646ee0ec7f164f68c110c6311d494ff0fc445136
Anton Logunov <antoshkaplus@gmail.com>
2019-11-16 18:23:50 -0800

    gogo

--- include/ant/graph/bellman_ford.hpp
+++ include/ant/graph/bellman_ford.hpp
@@ -1,58 +1,48 @@
 #pragma once
 
 #include "ant/core/core.hpp"
+#include "graph.hpp"
 
 namespace ant::graph {
 
-//for each vertex v in vertices:
-//  distance[v] := inf             // Initialize the distance to all vertices to infinity
-//  predecessor[v] := null         // And having a null predecessor
-//
-//        distance[source] := 0              // The distance from the source to itself is, of course, zero
-//
-//// Step 2: relax edges repeatedly
-//for i from 1 to size(vertices)-1:
-//for each edge (u, v) with weight w in edges:
-//if distance[u] + w < distance[v]:
-//distance[v] := distance[u] + w
-//        predecessor[v] := u
-//
-//// Step 3: check for negative-weight cycles
-//for each edge (u, v) with weight w in edges:
-//if distance[u] + w < distance[v]:
-//error "Graph contains a negative-weight cycle"
-//
-//return distance[], predecessor[]
+template <typename Graph, typename = std::enable_if_t<
+        is_directed_v<Graph> &&
+        is_vertex_id_index_v<Graph> &&
+        std::is_signed_v<Graph::VertexIdType> &&
+        std::is_arithmetic_v<typename Graph::EdgeValueType>>>
+void BellmanFord(const Graph& G, typename Graph::VertixIdType source) {
 
+    std::vector<typename Graph::EdgeValueType> distance(G.vertex_count(), std::numeric_limits<typename Graph::EdgeValueType>::max());
+    std::vector<typename Graph::VertixIdType> predecessor(G.vertex_count(), -1);
 
-// WeightFunc extracts weight from the graph edge value
-// now also need to get weight type itself
+    distance[source] = typename Graph::EdgeValueType {};
 
-// or maybe just restrict for now, make certain requirements for the graph
-
-// can we run adapter that will trasnform current edge value into another one ???
-
-// with adapters I can't do many changes
-
-
-template <typename Graph, typename = std::enable_if_t<is_vertex_id_index<T>::value>>
-void BellmanFord(const Graph& G, typename Graph::VertixId source) {
-
-
-
-    std::vector<WeightType> distances (G.vertix_count(), std::numeric_limits<WeightType>::min());
-
-
-
-    // somehow I need to get
-    // new vector for distances and predesessor
-
-    // Graph::VertixMap<>
-
-    for (auto v : G.vertices()) {
-
+    for (typename Graph::VertixIdType i = 0; i < G.vertex_count()-1; ++i) {
+        for (auto& e : G.edges()) {
+            // member functions are used as fetching all members all the time can be costly
+            // especially if not all used all the time
+            if (distance[e.source()] + e.value() < distance[e.target()]) {
+                distance[e.target()] = distance[e.source()] + e.value();
+                predecessor[e.target()] = e.source();
+            }
+        }
     }
 
+    bool negative_cycle = false;
+    for (auto& e : G.edges()) {
+        if (distance[e.source()] + e.value() < distance[e.target()]) {
+            negative_cycle = true;
+            break;
+        }
+    }
+
+    struct {
+        decltype(distance) distance;
+        decltype(predecessor) predecessor;
+        bool negative_cycle;
+    } res {std::move(distance), std::move(predecessor), negative_cycle};
+
+    return std::move(res);
 }
 
 
--- include/ant/graph/johnson.hpp
+++ include/ant/graph/johnson.hpp
@@ -1,7 +1,48 @@
 #pragma once
 
+#include "bellman_ford.hpp"
+
 namespace ant::graph {
 
+// all pairs shortest paths
+template <typename Graph, typename SupergraphBuilder, typename AllShortestPathsFunc, typename = std::enable_if_t<
+        is_directed_v<Graph> &&
+        std::is_arithmetic_v<typename Graph::EdgeValueType>>>
+void Johnson(Graph& graph, SupergraphBuilder&& builder, AllShortestPathsFunc&& all_shortest_paths_func) {
+
+    struct {
+        bool negative_cycle;
+        decltype(all_shortest_paths_func(std::declval<Graph>())) all_shortest_paths;
+    } res;
+
+
+    builder.Reset(graph);
+    auto q = builder.AddVertex();
+    for (auto& vertex : graph.vertices()) {
+        builder.AddEdge(q, vertex, typename Graph::EdgeValueType{});
+    }
+
+    {
+        auto subgraph = builder.Build();
+        auto result = BellmanFord(subgraph, q);
+        if (result.negative_cycle) {
+            res.negative_cycle = true;
+            return res;
+        }
+
+        for (auto& e : graph.edges()) {
+            e.value += result.distance[e.source()] - result.distance[e.target()];
+        }
+
+        res.negative_cycle = false;
+        res.all_shortest_paths = all_shortest_paths_func(graph);
+
+        for (auto& e : graph.edges()) {
+            e.value -= result.distance[e.source()] - result.distance[e.target()];
+        }
+    }
+    return res;
+}
 
 
 }
