dd21ebec27780c90eda05b1d5c5927ca016f8898
Anton Logunov <antoshkaplus@gmail.com>
2020-01-29 04:13:20 -0800

    gogo

--- include/ant/graph/model/adj_list/model.hpp
+++ include/ant/graph/model/adj_list/model.hpp
@@ -1,38 +1,63 @@
 #pragma once
 
+#pragma once
+
 #include "ant/core/core.hpp"
 
 namespace ant::graph::model::adj_list {
 
 // also provides edge descriptor
-template<typename Policy, typename VertexInfo, template <typename> class VertexType_, template <typename> class Mutator_>
-class Model_1_1 {
-    friend class Mutator_<Model_1_1>;
-    friend class VertexType_<Model_1_1>;
 
-    std::vector<VertexInfo> adjacent_;
+// once using iterators for edges and vertices, don't need to pass more stuff
+
+template <typename VertexValue>
+struct VertexInfo {
+    // maybe another iterator for next vertex, faster navigation
+    std::list<std::list<EdgeInfo>::iterator> adjacent_;
+    VertexValue value_;
+};
+
+template <typename EdgeValue>
+struct EdgeInfo {
+    std::list<EdgeInfo>::iterator descriptor;
+
+    std::list<VertexInfo>::iterator source;
+    std::list<VertexInfo>::iterator target;
+
+    std::array<std::list<VertexInfo>::iterator, 2> vertices;
+
+    EdgeValue value_;
+};
+
+
+template<typename Policy, typename VertexInfo, template <typename> class VertexType_, template <typename> class Mutator_>
+class Model {
+    friend class Mutator_<Model>;
+    friend class VertexType_<Model>;
+
+    std::list<VertexInfo> vertices_info_;
+    std::list<EdgeInfo> edges_info_;
 
 public:
-    using VertexDescriptor = typename VertexInfo::VertexDescriptor;
-    using VertexType = VertexType_<Model_1_1>;
-    using Mutator = Mutator_<Model_1_1>;
+    using VertexDescriptor = std::list<VertexInfo>::iterator;
+    using EdgeDescriptor = std::list
+    using VertexType = VertexType_<Model>;
+    using Mutator = Mutator_<Model>;
 
     auto vertices() {
-        return IteratorRange(
-                IndexVertexIterator(*this, 0),
-                IndexVertexIterator(*this, adjacent_.size()));
+        return IteratorRange(adjacent_.begin(), adjacent_.end());
     }
 
-    VertexType vertex(VertexDescriptor vertex_descriptor) {
-        return VertexType(*this, vertex_descriptor);
+    auto vertex(VertexDescriptor vertex_descriptor) {
+        return VertexType(vertex_descriptor);
     }
 
     auto edges() {
-
+        return IteratorRange(edges_.begin(), edges_.end());
     }
 
     auto edge(EdgeDescriptor edge_descriptor) {
-        return;
+        return EdgeType(edge_descriptor);
     }
 };
 
